1e9bef2460dacec962b9fdae3757ad2e2eb9db22
Pfruit99 <mora099999@gmail.com>
2022-03-07 18:35:21 -0500

    react router dom v6

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,30 +1,31 @@
 // usar comando rafce 
-
+import { render } from "react-dom";
 import React from 'react';
 
-
-import { BrowserRoute, Switch, router } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Layout  from '../containers/Layout';
 import  Login  from '../containers/Login';
-import '../styles/global.scss';
 import RecoveryPassword from '../containers/RecoveryPassword';
 
-const App = () => {
-  return (
 
-    //se inserta la estructura Br para encapsular toda la app
-    //ahora se usa un switch
-    <BrowserRoute>
-    <switch>
-      <Layout>
-      <router exact path="/" component={Home}></router>
-        <router exact path="/Login" component={Login}></router>
-        <router exact path="/RecoveryPassword" component={RecoveryPassword}></router>
-        <router component={NotFound}></router>
-      </Layout>
-    </switch>
-    </BrowserRoute>
-  );
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
+
+import '../styles/global.scss';
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Layout>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/RecoveryPassword" element={<RecoveryPassword />} />
+          <Route path="*" element={<NotFound />} />
+      </Routes>
+        </Layout>
+    </BrowserRouter>
+  )
 }
 
 export default App;
