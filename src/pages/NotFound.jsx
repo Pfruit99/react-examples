dea7b37cac25f63d230eb08067fa142258c8c3aa
Pfruit99 <mora099999@gmail.com>
2022-03-10 10:21:00 -0500

    se agrego e implemento react-player

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
+import ReactPlayer from 'react-player';
+import '../styles/video_react.scss';
+
+ 
+
+
 const NotFound = () => {
+  
   return (
-    <div>404 not found......</div>
-  )
-}
+    <div className='contenedor'>
+
+      <h1 className='title-error404'> 404 page not found......</h1>
+      <p className='title-error404-pharse'>meanwhile watch this video :)</p>
+      
+      <ReactPlayer
+        url='https://youtu.be/WCi2DLYE82A'
+        width='854px'
+        height= '480px'
+        controls
+        playing
+        muted
+        ClassName="react-player">
+      </ReactPlayer>
+
+
+    </div>
+      
+  );
+};
 
 export default NotFound;
